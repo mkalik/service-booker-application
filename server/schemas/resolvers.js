cdf1dcc66f1d87e08d1b4a16e5e3d923b2cdfc57
mkalik <mbadiankouyate@gmail.com>
2023-04-04 21:20:47 -0700

    added some stuff to models and created a skeleton for typdefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,10 @@
-const { AuthenticationError } = require("apollo-server-express");
-const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
+const { Ticket, User } = require('../models');
 const resolvers = {
-
+    Query: {},
+    Mutation: {},
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,21 @@
 const { gql } = require('apollo-server-express');
+const typeDefs = gql`
+    type User{
+        _id:ID
+        name: String
+        tickets:[Tickets]
+    }
+    type Ticket{
+        _id: ID
+        title: String
+        user:User
+    }
+    type Query{
 
-const typeDefs = gql;
+        me:User
+        #if we get to employee portion we can add something to query all users
+    }
+    type Mutation{}
+`;
 
 module.exports = typeDefs;
